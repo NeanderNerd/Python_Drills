493397cdff2e3c111b8e5d91a6f924f0d6e0d4e5
Christopher Garrick <CLGarrick@live.com>
2017-01-29 13:37:36 -0800

    first commit
    first project completed

---
+++ PyDrill_Datetime_27_idle.py
@@ -0,0 +1,27 @@
+##  Python:  2.7.13
+##
+##  Author:  Christopher Garrick
+##
+##  Purpose:  The Tech Academy - Python Course, Item 62
+##            Python Drill_Daytime_27_idle
+##            A program to determin if New York and London branches
+##            are open from the time in Portland.
+
+
+import datetime
+from datetime import datetime
+
+
+moment = int(datetime.now().hour)
+
+def Branches(moment):
+    if (moment + 3) < 9 or (moment + 3) > 21:
+        print('New York Branch Closed')
+    elif (moment + 3) > 9 or (moment + 3) < 21:
+        print('New York Branch Open')
+    if (moment + 8) < 9 or (moment + 8) > 21:
+        print('London Branch Closed')
+    elif (moment + 8) > 9 or (moment + 8) < 21:
+        print('London Branch Open')
+
+Branches(moment)
